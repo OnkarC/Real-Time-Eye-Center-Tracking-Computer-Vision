306c16108803ee032532907e9d8f97063570faf9
Onkar Chaubal <cbl.ovc@gmail.com>
2018-06-21 18:28:32 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,31 @@
+/* Headers */
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+#include <stdio.h>
+#include "centerTracking.h"
+#include "eyeList.h"
 
+using namespace std;
+using namespace cv;
+
+/* Functional Headers */
+void display(Mat frame, Rect& face, eyeList& allEyes, bool noFace);
+void findCenter(Mat frame, Rect& face, eyeList& allEyes);
+bool detectEyes(Mat frame, eyeList& eyes, Rect& face);
+void resize(Mat& input);
+
+/* Global Variables */
+CascadeClassifier face_cascade;
+CascadeClassifier eyes_cascade;
+
+
+/* main */
+int main( int argc, const char** argv ){
+ 
+  
+  
+  
+  
+}
